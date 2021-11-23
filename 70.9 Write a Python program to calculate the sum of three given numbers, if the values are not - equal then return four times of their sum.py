291694d0dda6d7ead41b8e1cebd3e59d35ae2ef2
Shyam83-oss <83969699+Shyam83-oss@users.noreply.github.com>
2021-11-23 21:16:22 +0530

    Create 70.9 Write a Python program to calculate the sum of three given numbers, if the values are not - equal then return four times of their sum.py

---
+++ 70.9 Write a Python program to calculate the sum of three given numbers, if the values are not - equal then return four times of their sum.py
@@ -0,0 +1,16 @@
+# By - Shyam Mandora
+# Date of Creation - 23-11-2021
+# Last Modified - 23-11-2021
+def sum_thrice(x, y, z):
+     sum = x + y + z
+      if x == y == z:
+      sum = sum * 3
+     return sum
+
+print(sum_thrice(1, 2, 3))
+print(sum_thrice(3, 3, 3))
+
+#output
+#6                                                                                                             
+#27
+
