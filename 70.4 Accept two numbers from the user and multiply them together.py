9852fea8c0181b00f3fc2b3a19befa8f6ed19b01
Shyam83-oss <83969699+Shyam83-oss@users.noreply.github.com>
2021-11-23 20:52:43 +0530

    Create 70.4 Accept two numbers from the user and multiply them together.py

---
+++ 70.4 Accept two numbers from the user and multiply them together.py
@@ -0,0 +1,7 @@
+# By - Shyam Mandora
+# Date of Creation - 23-11-2021
+# Last Modified - 23-11-2021
+a=int(input("enter first number : "))
+b=int(input("enter second number : "))
+print("Product of",a, "and",b,"is",a*b)
+
