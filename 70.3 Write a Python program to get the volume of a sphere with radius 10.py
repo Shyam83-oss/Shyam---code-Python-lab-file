e926bffcabc3dbe55ec9227c7bd94294d527bfcd
Shyam83-oss <83969699+Shyam83-oss@users.noreply.github.com>
2021-11-23 20:50:53 +0530

    Create 70.3 Write a Python program to get the volume of a sphere with radius 10.py

---
+++ 70.3 Write a Python program to get the volume of a sphere with radius 10.py
@@ -0,0 +1,8 @@
+# By - Shyam Mandora
+# Date of Creation - 23-11-2021
+# Last Modified - 23-11-2021
+
+vol= 1.33*3.14*10*10*10
+x="{:.2f}".format(vol)
+print("the volume of sphere of radius 10 is ",x)
+
