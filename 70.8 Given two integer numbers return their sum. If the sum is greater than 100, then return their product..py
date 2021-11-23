83a0738da28f975eb53afbfc00eb3e7f18c035c3
Shyam83-oss <83969699+Shyam83-oss@users.noreply.github.com>
2021-11-23 21:14:39 +0530

    Create 70.8 Given two integer numbers return their sum. If the sum is greater than 100, then return their product..py

---
+++ 70.8 Given two integer numbers return their sum. If the sum is greater than 100, then return their product..py
@@ -0,0 +1,10 @@
+# By - Shyam Mandora
+# Date of Creation - 23-11-2021
+# Last Modified - 23-11-2021
+
+#a=int(input("enter first number"))
+b=int(input("enter second number"))
+sum = a+b
+print("the sum of numner is ", sum )
+if sum > 100:
+  print(a*b)
